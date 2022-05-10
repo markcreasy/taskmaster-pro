d3928b94e6ac798fccc17479eced128df57c1c2a
Mark Creasy <mcreasy50@gmail.com>
2022-05-10 11:47:05 -0400

    custom UX

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,8 +4,6 @@ var tasks = {};
 var auditTask = function(taskEl) {
   // get date from task element
   var date = $(taskEl).find("span").text().trim();
-  // ensure it worked
-  console.log(date);
 
   // convert to moment object at 5:00pm
   var time = moment(date, "L").set("hour", 17);
@@ -191,7 +189,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -234,6 +232,20 @@ $(".card .list-group").sortable({
   scroll: false,
   tolerance: "pointer",
   helper: "clone",
+  activate: function(event){
+    $(this).addClass("dropover");
+    $(".bottom-trash").addClass("bottom-trash-drag");
+  },
+  deactivate: function(event){
+    $(this).removeClass("dropover");
+    $(".bottom-trash").removeClass("bottom-trash-drag");
+  },
+  over: function(event){
+    $(this).addClass("dropover-active");
+  },
+  out: function(event){
+    $(this).removeClass("dropover-active");
+  },
   update: function(event) {
     // array to store the task data in
     var tempArr = [];
@@ -271,7 +283,14 @@ $(".card .list-group").sortable({
 $("#trash").droppable({
   accept: ".card .list-group-item",
   tolerance: "touch",
+  over: function(){
+    $(".bottom-trash").addClass("bottom-trash-active");
+  },
+  out: function(){
+    $(".bottom-trash").removeClass("bottom-trash-active");
+  },
   drop: function(event, ui) {
+    $(".bottom-trash").removeClass("bottom-trash-active");
     ui.draggable.remove();
   }
 });
@@ -282,3 +301,9 @@ $("#trash").droppable({
 $("#modalDueDate").datepicker({
   minDate: 1
 });
+
+setInterval(function () {
+  $(".card .list-group-item").each(function(index, el) {
+    auditTask(el);
+  });
+}, 5000);
